07d72a8f6215934178b2e0809e3eafd5ed0705f1
sunilskuta <sunilskuta@gmail.com>
2026-02-07 15:03:20 +0530

    Add files via upload

---
+++ Tfbc.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB3iBWleWNMCdQS9F0HR1wHBBtHkeMawhg",
+  authDomain: "sjpss-f504b.firebaseapp.com",
+  projectId: "sjpss-f504b",
+  storageBucket: "sjpss-f504b.firebasestorage.app",
+  messagingSenderId: "227402980311",
+  appId: "1:227402980311:web:56b580456ab6b9c6758992",
+  measurementId: "G-LZ4YP9YPN6"
+};
